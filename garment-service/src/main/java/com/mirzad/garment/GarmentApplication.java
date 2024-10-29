2d5cb849f1f3edf42833c8383c560c36cb25e8d9
tamirzad <mirzadtahirovic00@outlook.com>
2024-10-29 23:21:43 +0100

    Initial commit

---
+++ garment-service/src/main/java/com/mirzad/garment/GarmentApplication.java
@@ -0,0 +1,13 @@
+package com.mirzad.garment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarmentApplication.class, args);
+	}
+
+}
---
+++ garment-service/src/test/java/com/mirzad/garment/GarmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirzad.garment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
